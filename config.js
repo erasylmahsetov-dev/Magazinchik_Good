5e11f1670e21147a3c19009db14f901462861c27
erasylmahsetov-dev <erasylmahsetov@gmail.com>
2026-09-15 20:42:10 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.SHOP_CONFIG = window.SHOP_CONFIG || {
+  SUPABASE_URL: 'https://rlizilvsewfelptyloth.supabase.co',
+  SUPABASE_ANON_KEY: 'PASTE_YOUR_SUPABASE_ANON_KEY_HERE',
+  STORAGE_BUCKET: 'product-images',
+  TELEGRAM_BOT_USERNAME: ''
+};
